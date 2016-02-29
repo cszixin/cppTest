a3c49e60c5a4086100a286fc4ef5b9cd9edce1fa
cszixin <844952162@qq.com>
2016-02-29 16:52:48 +0800

    make code more standardize

--- std_array/std_array.cpp
+++ std_array/std_array.cpp
@@ -1,16 +1,16 @@
 // std_array.cpp
-
+// copyright
+// author
 #include <iostream>
 #include <array>
-
-using namespace std;
-
-int main()
-{
-	array<int, 3> arr = {9, 8, 7};
-	cout << "Array size = " << arr.size() << endl;
-	for (auto i : arr)
-		cout << i << endl;
-
-	return 0;
+using  std::array;
+using  std::cout;
+using  std::cin;
+int main() {
+    array<int, 3> arr = {9, 8, 7};
+    cout << "Array size = " << arr.size() << endl;
+    for (auto i : arr) {
+      cout << i << endl;
+    }
+    return 0;
 }
