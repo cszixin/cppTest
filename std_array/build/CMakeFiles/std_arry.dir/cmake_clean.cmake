cf046675efca67801667cac7760143318ef68012
cszixin <844952162@qq.com>
2016-02-29 16:22:26 +0800

    c++11 array test

---
+++ std_array/build/CMakeFiles/std_arry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/std_arry.dir/std_array.cpp.o"
+  "std_arry.pdb"
+  "std_arry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/std_arry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
